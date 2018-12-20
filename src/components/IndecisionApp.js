1bbdf101b5a5b98b1ac45518786906418486e020
Jem Alvarez <jemalvarez22@gmail.com>
2018-12-20 15:53:08 -0500

    Initial commit

---
+++ src/components/IndecisionApp.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import AddOption from './AddOption'
+import Header from './Header'
+import Action from './Action'
+import Options from './Options'
+import OptionModal from './OptionModal'
+
+class IndecisionApp extends React.Component {
+    state = {
+        options: [],
+        selectedOption: undefined
+    }
+    handleModalClose = () => {
+        this.setState(() => ({ selectedOption: undefined }))
+    }
+    handleDeleteOptions = () => {
+        this.setState(() => ({ options: [] }))
+    }
+    handleDeleteOption = (option) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((opt) => option !== opt)
+        }))
+    }
+    handlePick = () => {
+        const randomNum = Math.floor(Math.random() * this.state.options.length)
+        const option = this.state.options[randomNum]
+        this.setState(() => ({ selectedOption: `${randomNum + 1} - ${option}` }))
+    }
+    hadnleAddOption = (option) => {
+        if (!option) {
+            return 'Enter valid value to add option'
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists'
+        }
+
+        this.setState((prevState) => ({ options: prevState.options.concat(option) }))
+    }
+    componentDidMount = () => {
+        try {
+            const json = localStorage.getItem('options')
+            const options = JSON.parse(json)
+            if (options) {
+                this.setState(() => ({ options }))
+            }
+        } catch (e) {
+            // Do nothing
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options)
+            localStorage.setItem('options', json)
+        }
+    }
+    render() {
+        const subtitle = 'Put your life in the hands of a computer'
+
+        return (
+            <div>
+                <Header
+                    subtitle={subtitle}
+                />
+                <div className="container">
+                    <Action
+                        hasOptions={this.state.options.length > 0}
+                        handlePick={this.handlePick}
+                    />
+                    <div className="widget">
+                        <Options
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            options={this.state.options}
+                            handleDeleteOption={this.handleDeleteOption}
+                        />
+                        <AddOption
+                            hadnleAddOption={this.hadnleAddOption}
+                        />
+                    </div>
+                </div>
+                <OptionModal
+                    selectedOption={this.state.selectedOption}
+                    handleModalClose={this.handleModalClose}
+                />
+            </div>
+        )
+    }
+}
+
+export default IndecisionApp
